8eed2312c8d7b77c58ba973548ebdfd3f14fd92f
ssmtkmt <tokumoto.susumu@nifty.com>
2025-10-06 20:11:12 +0900

    Initial commit

---
+++ calculate_average.py
@@ -0,0 +1,10 @@
+def calculate_average(numbers: list) -> float:
+    # 合計値を初期化
+    total = 0.0
+
+    # リストの各数値を合計値に加算
+    for number in numbers:
+        total += number
+
+    # 合計値をリストの長さで割って平均値を計算
+    return total / len(numbers)
